42966988d18e938f0dc4b1fbb0b6c166e1081f7f
Adnan Mamajiwala <rawkz.star@gmail.com>
2021-08-09 10:43:30 -0500

    adds navigation and basic structuring

--- demo-client-ng-app/src/app/app.module.ts
+++ demo-client-ng-app/src/app/app.module.ts
@@ -4,10 +4,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
--- demo-client-server-employee-svc/src/main/java/com/example/employee/EmployeeController.java
+++ demo-client-server-employee-svc/src/main/java/com/example/employee/EmployeeController.java
@@ -1,19 +1,14 @@
 package com.example.employee;
 
 
-import com.example.employee.models.Address;
 import com.example.employee.models.Employee;
 import com.example.employee.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.validation.Valid;
 import java.util.List;
-
-import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -33,16 +28,5 @@ public class EmployeeController {
     public Employee save(@Valid Employee employee) {
         return service.save(employee);
     }
-
-    @GetMapping(value = "/address")
-    public Address[] getArticles() {
-        return this.webClient
-                .get()
-                .uri("http://localhost:8090/addresses")
-                .retrieve()
-                .bodyToMono(Address[].class)
-                .block();
-    }
-
 
 }
